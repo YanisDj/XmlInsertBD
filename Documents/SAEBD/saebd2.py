61bb153f357892520c1ee06a6d0c341cecc4b900
mghlouci <merwan.ghlouci@gmail.com>
2023-04-11 02:23:45 +0200

    fix bug

--- Documents/SAEBD/saebd2.py
+++ Documents/SAEBD/saebd2.py
@@ -10,22 +10,23 @@ import mysql.connector
 
 # Connexion à la base de données
 db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="dblp"
+    host="localhost",
+    user="root",
+    password="",
+    database="dblp"
 )
 
 cursor = db.cursor()
-
 
 # Parsing du fichier XML
 tree = ET.parse("dblpl5000.xml")
 root = tree.getroot()
 
-nbauteur=0
+nbauteur = 0
+idPub = 0
 # Itération à travers tous les éléments 'data' du fichier XML
 for data in root.findall('data'):
+    idPub = idPub + 1
     # Récupération des attributs du noeud 'data'
     mdate = data.get('mdate')
     key_id = data.get('key')
@@ -51,80 +52,75 @@ for data in root.findall('data'):
 
     # Insertion des données dans la table 'publications'
     cursor.execute("""
-        INSERT INTO publications (title, year, venue, nauthor, type)
-        VALUES (%s, %s, %s, NULL, %s)
-    """, (title, year, publisher, "conference"))
+        INSERT INTO publications (id, title, year, venue, nauthor, type)
+        VALUES (%s, %s, %s, %s, NULL, %s)
+    """, (idPub, title[:150], year, publisher, "conference"))
 
-    # Récupération de l'identifiant de la dernière insertion
-    publication_id = cursor.lastrowid
+
 
     # Insertion des données dans la table 'publicationdetails'
     cursor.execute("""
         INSERT INTO publicationdetails (publication_id, authors, mdate, key_id)
         VALUES (%s, NULL, %s, %s)
-    """, (publication_id, mdate, key_id))
+    """, (idPub, mdate, key_id))
 
-    # Itération à travers tous les éléments 'author' du noeud 'data'
-    for author in data.findall('author'):
-        
-        # Récupération de l'attribut 'orcid' de l'élément 'author'
-        orcid = author.get('orcid')
-
-        # Récupération du nom de l'auteur
-        name = author.text
-
-        # Insertion des données dans la table 'authors'
-        cursor.execute("""
-            INSERT INTO authors (name, firstpaper, lastpaper, affiliation)
-            VALUES (%s, NULL, NULL, NULL)
-        """, (name,))
-
-        # Récupération de l'identifiant de la dernière insertion
-        author_id = cursor.lastrowid
-
-        # Insertion des données dans la table 'publicationauthors'
-        cursor.execute("""
-            INSERT INTO publicationauthors (author_id, publication_id)
-            VALUES (%s, %s)
-        """, (author_id, publication_id))
-
-        # Itération à travers tous les autres éléments 'author' du noeud 'data'
-        for coauthor in data.findall('author'):
-            # Récupération de l'attribut 'orcid' de l'élément 'author'
-            coauthor_orcid = coauthor.get('orcid')
-
-            # Récupération du nom de l'auteur
-            coauthor_name = coauthor.text
-            
-            # Vérification si l'auteur courant est différent de l'auteur collaborateur
-            if name != coauthor_name:
-                # Insertion des données dans la table 'authors'
-                cursor.execute("""
-                    INSERT INTO authors (name, firstpaper, lastpaper, affiliation)
-                    VALUES (%s, NULL, NULL, NULL)
-                """, (coauthor_name,))
-    
-                # Récupération de l'identifiant de la dernière insertion
-                coauthor_id = cursor.lastrowid
-    
-                # Insertion des données dans la table 'publicationauthors'
-                cursor.execute("""
-                    INSERT INTO publicationauthors (author_id, publication_id)
-                    VALUES (%s, %s)
-                """, (coauthor_id, publication_id))
-    
-                # Insertion des données dans la table 'authorcoauthors'
-                cursor.execute("""
-                    INSERT INTO authorcoauthors (author1_id, author2_id)
-                    VALUES (%s, %s)
-                """, (author_id, coauthor_id))
+    print(idPub)
+    # # Itération à travers tous les éléments 'author' du noeud 'data'
+    # for author in data.findall('author'):
+    #
+    #     # Récupération de l'attribut 'orcid' de l'élément 'author'
+    #     orcid = author.get('orcid')
+    #
+    #     # Récupération du nom de l'auteur
+    #     name = author.text
+    #
+    #     # Insertion des données dans la table 'authors'
+    #     cursor.execute("""
+    #         INSERT INTO authors (name, firstpaper, lastpaper, affiliation)
+    #         VALUES (%s, NULL, NULL, NULL)
+    #     """, (name,))
+    #
+    #     # Récupération de l'identifiant de la dernière insertion
+    #     author_id = cursor.lastrowid
+    #
+    #     # Insertion des données dans la table 'publicationauthors'
+    #     cursor.execute("""
+    #         INSERT INTO publicationauthors (author_id, publication_id)
+    #         VALUES (%s, %s)
+    #     """, (author_id, publication_id))
+    #
+    #     # Itération à travers tous les autres éléments 'author' du noeud 'data'
+    #     for coauthor in data.findall('author'):
+    #         # Récupération de l'attribut 'orcid' de l'élément 'author'
+    #         coauthor_orcid = coauthor.get('orcid')
+    #
+    #         # Récupération du nom de l'auteur
+    #         coauthor_name = coauthor.text
+    #
+    #         # Vérification si l'auteur courant est différent de l'auteur collaborateur
+    #         if name != coauthor_name:
+    #             # Insertion des données dans la table 'authors'
+    #             cursor.execute("""
+    #                 INSERT INTO authors (name, firstpaper, lastpaper, affiliation)
+    #                 VALUES (%s, NULL, NULL, NULL)
+    #             """, (coauthor_name,))
+    #
+    #             # Récupération de l'identifiant de la dernière insertion
+    #             coauthor_id = cursor.lastrowid
+    #
+    #             # Insertion des données dans la table 'publicationauthors'
+    #             cursor.execute("""
+    #                 INSERT INTO publicationauthors (author_id, publication_id)
+    #                 VALUES (%s, %s)
+    #             """, (coauthor_id, publication_id))
+    #
+    #             # Insertion des données dans la table 'authorcoauthors'
+    #             cursor.execute("""
+    #                 INSERT INTO authorcoauthors (author1_id, author2_id)
+    #                 VALUES (%s, %s)
+    #             """, (author_id, coauthor_id))
 
 # Validation de la transaction
 db.commit()
 
 db.close()
-
-
-
-
-
