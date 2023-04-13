b4ee47f50e0c01e1e3b76415b2ddae37034bbaaa
mghlouci <merwan.ghlouci@gmail.com>
2023-04-13 02:50:47 +0200

    remplissage table coauthors

--- Documents/SAEBD/saebd (2).py
+++ Documents/SAEBD/saebd (2).py
@@ -12,7 +12,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="caca123",
     database="dblp"
 )
 
@@ -25,7 +25,7 @@ root = tree.getroot()
 nbauteur = 0
 idPub = 0
 idauthor = 0
-idcoauthor=0
+idcoauthor = 0
 # Itération à travers tous les éléments 'data' du fichier XML
 for data in root.findall('data'):
     idPub = idPub + 1
@@ -58,8 +58,6 @@ for data in root.findall('data'):
         VALUES (%s, %s, %s, %s, NULL, %s)
     """, (idPub, title[:150], year, publisher, "conference"))
 
-
-
     # Insertion des données dans la table 'publicationdetails'
     cursor.execute("""
         INSERT INTO publicationdetails (publication_id, authors, mdate, key_id)
@@ -79,15 +77,30 @@ for data in root.findall('data'):
         cursor.execute("""
             INSERT INTO authors (id,name, firstpaper, lastpaper, affiliation)
             VALUES (%s,%s, NULL, NULL, NULL)
-        """, (idauthor,name))
-
-        
+        """, (idauthor, name[:40]))
 
         # Insertion des données dans la table 'publicationauthors'
         cursor.execute("""
             INSERT INTO publicationauthors (author_id, publication_id)
             VALUES (%s, %s)
         """, (idauthor, idPub))
+
+        # Remplissage des données dans la table 'coauthors'
+        cursor.execute("""select * from publicationauthors""")
+        results = cursor.fetchall()
+        for i in range(len(results)):
+            for j in range(i + 1, len(results)):
+                a = results[i]
+                b = results[j]
+                if a[1] == b[1] & a[0] != b[0]:
+                    sql = "SELECT * FROM coauthors WHERE author_id = %s AND coauthor_id = %s"
+                    cursor.execute(sql, (a[0], b[0]))
+                    if cursor.fetchone() is None:
+                        sql = "INSERT INTO coauthors (author_id, coauthor_id) VALUES (%s, %s)"
+                        cursor.execute(sql, (a[0], b[0]))
+
+
+
 
         # # Itération à travers tous les autres éléments 'author' du noeud 'data'
         # for coauthor in data.findall('author'):
@@ -96,7 +109,7 @@ for data in root.findall('data'):
 
         #     # Récupération du nom de l'auteur
         #     coauthor_name = coauthor.text
-            
+
         #     # Vérification si l'auteur courant est différent de l'auteur collaborateur
         #     if name != coauthor_name:
         #         idcoauthor=idauthor+1
@@ -105,15 +118,13 @@ for data in root.findall('data'):
         #             INSERT INTO authors (id,name, firstpaper, lastpaper, affiliation)
         #             VALUES (%s,%s, NULL, NULL, NULL)
         #         """, (idcoauthor,coauthor_name))
-    
-               
-    
+
         #         # Insertion des données dans la table 'publicationauthors'
         #         cursor.execute("""
         #             INSERT INTO publicationauthors (author_id, publication_id)
         #             VALUES (%s, %s)
         #         """, (idcoauthor, idPub))
-    
+
         #         # Insertion des données dans la table 'authorcoauthors'
         #         cursor.execute("""
         #             INSERT INTO coauthors (author_id, coauthor_id)
@@ -124,8 +135,3 @@ for data in root.findall('data'):
 db.commit()
 
 db.close()
-
-
-
-
-
